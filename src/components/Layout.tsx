7818f15405432da813bbaa6509fc8ae3e457b1ba
EnderDev <kieran@dothq.org>
2023-05-12 02:37:51 +0100

    🐛 Fix path of favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,7 +19,7 @@ export const Layout = ({
 				<title>{title ? `${title} - ` : `` + "Applied Management Solutions"}</title>
 				<meta name="description" content={description || "Applied Management Solutions"} />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="./favicon.ico" />
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header />
 			<main id={"main-content"} className={`page-${page}`}>
